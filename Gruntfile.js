72127a6ae0dec11ab51aaf9c6a355ce3a6c69715
AJ Hsu <ajhsu@kkbox.com>
2016-01-20 20:08:50 +0800

    Remove samples-related grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,11 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    host_config: grunt.file.readJSON('.host_config'),
-    compass: {
-      dist: {
-        options: {
-          sassDir: 'examples/sass',
-          cssDir: 'examples/css',
-          environment: 'production'
-        }
-      }
-    },
     connect: {
       server: {
         options: {
           port: 8000
         }
-      }
-    },
-    'ftp-deploy': {
-      build: {
-        auth: {
-          host: '<%- host_config.host %>',
-          port: '<%- host_config.port %>'
-        },
-        src: '.',
-        dest: '<%- host_config.directory %>',
-        exclusions: [
-          '**/.*',
-          '.*',
-          'bower_components',
-          'node_modules'
-        ]
       }
     },
     jshint: {
@@ -49,14 +23,6 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      sass: {
-        files: ['examples/sass/*.sass'],
-        tasks: ['compass'],
-        options: {
-          livereload: true,
-          spawn: false
-        },
-      },
       test: {
         files: ['src/color-thief.js'],
         tasks: ['jshint']
@@ -64,14 +30,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-ftp-deploy');
 
-
-  grunt.registerTask('default', ['compass', 'connect', 'watch']);
-  grunt.registerTask('build', ['compass', 'jshint', 'uglify']);
+  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('build', ['jshint', 'uglify']);
 };
